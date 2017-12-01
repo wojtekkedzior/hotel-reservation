77cfe2ec98fe3d12e00e1c643cf5809f88bd2166
Wojtek <wowaked@gmail.com>
2017-12-01 21:01:11 +0100

    note

--- src/main/java/hotelreservation/controller/ReservationController.java
+++ src/main/java/hotelreservation/controller/ReservationController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -37,12 +36,10 @@ public class ReservationController {
 		return "addReservation";
 	}
 	
-	@RequestMapping(value = "/addReservation/{id}", method=RequestMethod.GET) 
+	//Rename from add to edit
+	@RequestMapping(value = "/addReservation/{id}") 
 	public String getReservationModel(Model model, @PathVariable int id) {
-		model.addAttribute("reservation", new Reservation());
-		 
-		Reservation reservation = bookingService.getReservation(id);
-		model.addAttribute("reservation", reservation);
+		model.addAttribute("reservation", bookingService.getReservation(id));
 		return "addReservation";
 	} 
 
