cfa912cd5c8ef999757b45d1647708588e61866e
Wojtek <wowaked@gmail.com>
2017-12-03 22:41:34 +0100

    comments

--- src/main/java/hotelreservation/model/RoomRate.java
+++ src/main/java/hotelreservation/model/RoomRate.java
@@ -49,4 +49,6 @@ public class RoomRate {
 		this.day = day;
 	}
 	 
+	//TODO implement toString() and hash() and debug why in the addReseration.html the id has to be the value and the value be the id.  
+	//The idea is that either toString() or hash() are not returning the correct object based on the value, which after all is very repetative.
 }
