2f4b33d9b79e60a158805d4a28f32ae36c087633
Wojtek <wowaked@gmail.com>
2018-04-15 11:13:51 +0200

    fix saving room rates

--- src/main/java/hotelreservation/controller/RoomController.java
+++ src/main/java/hotelreservation/controller/RoomController.java
@@ -159,7 +159,9 @@ public class RoomController {
 
 	@PostMapping("/addRoomRate")
 	public ModelAndView addRoomRate(@ModelAttribute RoomRate roomRate, BindingResult bindingResult) {
-		log.info(bindingResult.toString());
+		if(bindingResult.hasErrors()) {
+			log.error(bindingResult.toString());
+		}
 		roomService.createRoomRate(roomRate);
 		return new ModelAndView("redirect:/roomRate/" + roomRate.getId());
 	}
