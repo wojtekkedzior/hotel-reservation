1a23801b34b5daed8460e22122ec9028fd51f0da
Wojtek <wowaked@gmail.com>
2018-05-16 15:44:03 +0200

    ada

--- src/main/java/hotelreservation/WebMvcConfig.java
+++ src/main/java/hotelreservation/WebMvcConfig.java
@@ -46,7 +46,6 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/reservationDashBoard").setViewName("reservationDashBoard");
     }
     
 //    @Override
