5cfed4ef63a1fa0f8deb9b086ac6489ddf32a3ef
Wojtek <wowaked@gmail.com>
2018-07-10 20:01:36 +0200

    imports

--- src/main/java/hotelreservation/model/finance/Payment.java
+++ src/main/java/hotelreservation/model/finance/Payment.java
@@ -8,7 +8,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
 import hotelreservation.model.Charges;
 import hotelreservation.model.Reservation;
