bb12a5ba613a692c732fa687068a1e102ec02d70
Kedzior, Wojtek <wowaked@gmail.com>
2019-04-10 16:00:00 +0200

    trying to use mocks for controller tests

--- src/test/java/hotelreservation/controller/AdminControllerTest.java
+++ src/test/java/hotelreservation/controller/AdminControllerTest.java
@@ -13,33 +13,80 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import hotelreservation.Application;
+import hotelreservation.WebSecurityConfig;
 import hotelreservation.model.Privilege;
 import hotelreservation.service.MyUserDetailsService;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = { Application.class, MyUserDetailsService.class })
-@DataJpaTest
-@AutoConfigureMockMvc
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = { MyUserDetailsService.class })
+//@DataJpaTest
+//@AutoConfigureMockMvc
+//@SpringBootTest
+//@WebMvcTest
+
+//@ContextConfiguration
+@WebAppConfiguration
+
+//@Import(MyUserDetailsService.class)
+//@WebMvcTest(includeFilters = @Filter(classes = EnableWebSecurity.class))
+//@Import(WebSecurityConfig.class)
+@ContextConfiguration(classes = Application.class)
+//@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class AdminControllerTest extends BaseControllerSetup {
 
-	@Autowired
-	private MockMvc mvc;
+//	@Autowired
+//	private MockMvc mvc;
 
-	@Before
-	public void setup() {
-	}
+//	@Before
+//	public void setup() {
+////		 mvc = MockMvcBuilders
+////	                .webAppContextSetup(context)
+////	                .apply(springSecurity()) // enable security for the mock set up
+////	                .build();
+//	}
 	
+	  @Autowired
+	    private WebApplicationContext context;
+
+	    private MockMvc mvc;
+
+	    @Before
+	    public void setup() {
+	        mvc = MockMvcBuilders
+	                .webAppContextSetup(context)
+	                .apply(SecurityMockMvcConfigurers.springSecurity())
+	                .build();
+	    }
+
+	    
+	    
+	
+//    @Autowired
+//    private WebApplicationContext context;
+
+
 	@Override
 	Collection<Privilege> getPrivilegesForReceptionist() {
 		Collection<Privilege> receptionistPrivileges = new ArrayList<Privilege>();
@@ -79,6 +126,7 @@ public class AdminControllerTest extends BaseControllerSetup {
 	
 	@Test
 	@WithUserDetails("manager")
+//	 @WithMockUser(value = "test", password = "pass")
 	public void testManagerCanAccessAdmin() throws Exception {
 		mvc.perform(get("/admin")).andExpect(status().isOk());
 	}
