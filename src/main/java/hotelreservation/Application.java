293898671c6cea517b5badf3bcbcc2d05f8f4a1f
Wojtek <wowaked@gmail.com>
2019-09-26 18:08:24 +0200

    test ssh key

--- src/main/java/hotelreservation/Application.java
+++ src/main/java/hotelreservation/Application.java
@@ -28,6 +28,4 @@ public class Application extends org.springframework.boot.web.servlet.support.Sp
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
 	}
-	
-	
 }
